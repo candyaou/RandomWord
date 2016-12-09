80da63c3f6bf8810374914a027f8e28a79c865a5
Paspana Assarasee <paspanaassarasee@Paspanas-MacBook-Air.local>
2016-12-09 15:43:08 +0700

    Finish Random word V.1.0

--- RandomWord/ViewController.swift
+++ RandomWord/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    
+    @IBOutlet weak var wordField: UITextField!
+    @IBOutlet weak var headLabel: UILabel!
+    @IBOutlet weak var wordSet: UILabel!
+    @IBOutlet weak var result: UILabel!
+    
+    var setOfWord = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        wordField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //init String to store text
+        let word = textField.text!
+        
+        //Add word to set of word
+        setOfWord.append(word)
+        
+        //print set of word
+        wordSet.text = setOfWord.joined(separator: "\t")
+        print(setOfWord)
+        
+        //Delete text from textField
+        textField.text = ""
+    }
+    
+    @IBAction func getWord(_ sender: UIButton) {
+        //random result
+        let randomNumber = Int(arc4random_uniform(UInt32(setOfWord.count)))
+        print(setOfWord[randomNumber])
+        result.text = setOfWord[randomNumber]
+    }
 
 }
 
